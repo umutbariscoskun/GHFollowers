e69cb3c626cf0f578e9bb785c99f00f7ca614b6b
umutbariscoskun <umutcoskun2017@gmail.com>
2022-11-17 20:54:51 +0300

    Followers and user model with codable Structs

---
+++ GHFollowers/Models/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Umut Barış Çoşkun on 17.11.2022.
+//
+
+import Foundation
+
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
